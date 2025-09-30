42dd09e7836c043fb4323be4718afe26bec40cd5
Sumit Kumar Nandy <146019889+nandysumit91@users.noreply.github.com>
2025-10-01 03:30:00 +0530

    Update creditRoutes.js

--- server/routes/creditRoutes.js
+++ server/routes/creditRoutes.js
@@ -1,10 +1,10 @@
-import express from "express"
-import { getPlans, purchasePlan } from "../controllers/creditController.js"
-import { protect } from "../middlewares/auth.js"
+import express from "express";
+import { getPlans, purchasePlan } from "../controllers/creditController.js";
+import { protect } from "../middlewares/auth.js";
 
-const creditRouter = express.Router()
+const creditRouter = express.Router();
 
-creditRouter.get('/plan', getPlans)
-creditRouter.post('/purchase', protect, purchasePlan)
+creditRouter.get("/plan", getPlans);
+creditRouter.post("/purchase", protect, purchasePlan);
 
-export default creditRouter
+export default creditRouter;
